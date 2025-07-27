f3f451d0ca97ff8446841f186cd3cf5ae142da53
Bharat Khatoliya <kha.bharat9098@gmail.com>
2025-07-27 20:19:20 +0530

    update spec - rename method name and retunr 0 if empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,11 +1,9 @@
 require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
-  describe '.sayHello' do
-    context 'when input is empty return string' do
-      it 'returns name successfully' do
-        expect(StringCalculator.sayHello).to eq("Hello Bharat Khatoliya")
-      end
+  describe '.add' do
+      it 'returns 0 for empty string' do
+      expect(StringCalculator.add("")).to eq(0)
     end
   end
 end
