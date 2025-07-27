d85f3da3b32fa761fd9083b1ff163d6ecbda67ac
Bharat Khatoliya <kha.bharat9098@gmail.com>
2025-07-27 20:21:49 +0530

    update specs to handle more cases

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,8 +2,16 @@ require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
-      it 'returns 0 for empty string' do
+    it 'returns 0 for empty string' do
       expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself if only one number is passed' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two comma separated numbers' do
+      expect(StringCalculator.add("1,5")).to eq(6)
     end
   end
 end
