7f1645a38c919333ec76370e54cdaed6eaa81591
Bharat Khatoliya <kha.bharat9098@gmail.com>
2025-07-27 21:56:30 +0530

    update method to handle custom delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,11 +1,20 @@
-# string_calculator.rb
-
+require 'pry'
 class StringCalculator
   def self.add(input)
     return 0 if input.strip.empty?
 
     numbers = []
-    numbers = input.split(/,|\n/).map(&:to_i)
+     if input.start_with?("//")
+      # Custom delimiter syntax: e.g. "//;\n1;2"
+      header, numbers_str = input.split("\n", 2)
+      custom_delimiter = header[2..]
+      delimiter_regex = Regexp.escape(custom_delimiter)
+      numbers = numbers_str.split(/#{delimiter_regex}/).map(&:to_i)
+    else
+      # Default delimiters: comma and newline
+      numbers = input.split(/,|\n/).map(&:to_i)
+    end
+
     numbers.sum
   end
 end
